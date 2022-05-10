4baa8ba95c876d89c26a171e5b415eb32b2f7bff
runehoejlund <s173965@student.dtu.dk>
2022-05-10 23:59:14 +0200

    hetereostructure calculations with own bbs

---
+++ independent_calculations/hetereostructure/interpolate_building_blocks.py
@@ -0,0 +1,2 @@
+from gpaw.response.qeh import interpolate_building_blocks
+interpolate_building_blocks(BBfiles=['MoTe2', 'WS2', 'WSe2'], BBmotherfile='BN')
--- independent_calculations/interpolate_building_blocks.py
+++
@@ -1,2 +0,0 @@
-from gpaw.response.qeh import interpolate_building_blocks
-interpolate_building_blocks(BBfiles=['WSe2'], BBmotherfile='MoS2')
