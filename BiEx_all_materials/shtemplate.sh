53405083cc94e236f4cdc3bbef557cd1976e83e1
runehoejlund <s173965@student.dtu.dk>
2022-09-30 10:36:49 +0200

    updated shtemplate

--- BiEx_all_materials/shtemplate.sh
+++ BiEx_all_materials/shtemplate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH --mail-type=NONE
-#SBATCH --mail-user=s183774@student.dtu.dk  # The default value is the submitting user.
 #SBATCH --partition=%(partition)s
-#SBATCH -N 1      # Minimum of 1 node
-#SBATCH -n %(cores)s     # 24 MPI processes per node, 48 tasks in total, appropriate for xeon24 nodes
+#SBATCH -N 1      # Number of nodes
+#SBATCH -n %(cores)s     # Total number of tasks
 #SBATCH --time=0-%(hours)s:00:00
 #SBATCH --output=%(out)s #name
 #SBATCH --error=%(err)s  #name
+
+%(setup)s
 
 module purge
 module add intel
