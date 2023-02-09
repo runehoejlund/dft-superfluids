5f2ba377bb2c35d01c90cb942c0fba76c89a5865
runehoejlund <s173965@student.dtu.dk>
2023-02-09 10:06:35 +0100

    cleanup

--- main_results/02-qeh-calculations/002.py
+++
@@ -1,7 +0,0 @@
-out = np.load('./materials_iso_stable.npz')
-h_iso = out['h_iso'].flatten()
-e_iso = out['e_iso'].flatten()
-e_avg_vec = out['e_avg_vec']
-h_avg_vec = out['h_avg_vec']
-m_e = e_avg_vec[e_iso]
-m_h = e_avg_vec[h_iso]
